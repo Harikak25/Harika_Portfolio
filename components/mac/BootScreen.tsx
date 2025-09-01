ee90e9f4c7724080f4d7948ed84671eb906e20fe
HarikaKoduri <harikabhavani.hb@gmail.com>
2025-09-01 15:51:24 -0500

    Enable dev branch preview deploy; fix logo sizing

--- components/mac/BootScreen.tsx
+++ components/mac/BootScreen.tsx
@@ -13,7 +13,7 @@ export default function BootScreen() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
       <div className="flex flex-col items-center gap-6">
-        <img src="/icons/apple-logo.png" alt="logo" className="h-20 w-20 opacity-90" />
+        <img src="/icons/apple-logo.png" alt="logo" className="h-20 w-auto opacity-90 object-contain"/>
         <div className="w-64 h-2 bg-white/10 rounded-full overflow-hidden">
           <div className="h-full bg-white/80 transition-[width] duration-100 ease-linear" style={{ width: `${p}%` }} />
         </div>
